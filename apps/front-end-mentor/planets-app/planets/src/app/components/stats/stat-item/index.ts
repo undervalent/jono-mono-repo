2eef75feb92878840e04efebefb62ab6ef3935c8
Jon Ochoa <undervalent@gmail.com>
2023-07-29 09:31:15 -0700

    feat(new setup): remove old files

--- apps/front-end-mentor/planets-app/planets/src/app/components/stats/stat-item/index.ts
+++
@@ -1,1 +0,0 @@
-export * from "./stat-item.component";
--- apps/front-end-mentor/planets-app/planets/src/app/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export const getPath = (path: string) => {
-  const planetName = path.split("/");
-  return planetName[planetName.length - 1];
-};
